038fb36da7f8028e4d0b6e8273199a3d867a1401
Phil <pamramfam@gmail.com>
2023-09-11 09:46:22 -0400

    Added functionality to dark mode toggle button

---
+++ script.js
@@ -0,0 +1,26 @@
+// VARIABLES 
+const hourEl = document.querySelector('.hour'); 
+const minuteEl = document.querySelector('.minute'); 
+const secondEl = document.querySelector('.second'); 
+const timeEl = document.querySelector('.time'); 
+const dateEl = document.querySelector('.date'); 
+const toggleEl = document.querySelector('.toggle'); 
+
+// DAYS AND MONTHS ARRAYS 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]; 
+
+// TOGGLE ELEMENT 
+toggleEl.addEventListener('click', (e) => {
+    const html = document.querySelector('html'); 
+    if (html.classList.contains('dark')) { 
+        html.classList.remove('dark'); 
+        e.target.innerHTML = 'Dark mode'; 
+    } else { 
+        html.classList.add('dark'); 
+        e.target.innerHTML = 'Light mode'; 
+    }
+})
+
+
+
