f374ad2114f6263a7179bf0bd4240b0275f3d135
Phil <pamramfam@gmail.com>
2023-09-11 12:03:47 -0400

    updated README. Updated js file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # themeClockDemo
 Inspired by this dribbble shot: 
-https://dribbble.com/shots/5958443/clock
+https://dribbble.com/shots/5958443/cloc
+StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+`k
--- script.js
+++ script.js
@@ -23,4 +23,36 @@ toggleEl.addEventListener('click', (e) => {
 })
 
 
+// CLOCK 
+function setTime() { 
+    const time = new Date(); 
+    console.log(time); 
+    const month = time.getMonth(); 
+    const day = time.getDay(); 
+    const hours = time.getHours(); 
+    const hoursForClock = hours % 12; 
+    const minutes = time.getMinutes(); 
+    const seconds = time.getSeconds(); 
 
+    // NEEDS OF CLOCK 
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+
+    // DATE and TIME 
+    
+    
+}
+
+
+// Retrieved from StackOverflow - link in README file
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+ 
+
+setTime(); 
+
+setInterval(setTime, 1000); 
