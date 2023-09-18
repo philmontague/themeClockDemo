1840b3d8dcf0c75ef8e3cd9d7d1e36df29201f1e
Phil <pamramfam@gmail.com>
2023-09-18 17:59:57 -0400

    updated JS file

--- script.js
+++ script.js
@@ -29,20 +29,22 @@ function setTime() {
     console.log(time); 
     const month = time.getMonth(); 
     const day = time.getDay(); 
+    const date = time.getDate(); 
     const hours = time.getHours(); 
     const hoursForClock = hours % 12; 
     const minutes = time.getMinutes(); 
     const seconds = time.getSeconds(); 
+    const ampm = hours >= 12 ? 'PM' : 'AM'; 
 
-    // NEEDS OF CLOCK 
+    // CLOCK NEEDLES
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
 
 
     // DATE and TIME 
-    
-    
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`; 
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`; 
 }
 
 
